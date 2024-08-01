cdb174b4bbdf87d511424a483dcee036a2c41ac2
Filip <filip.a.dabrowski@gmail.com>
2024-08-01 09:32:47 +0200

    Added comments to class Livre

--- src/main/java/entites/Livre.java
+++ src/main/java/entites/Livre.java
@@ -2,14 +2,19 @@ package entites;
 
 import jakarta.persistence.*;
 
+// L'annotation @Entity spécifie que la classe doit être traitée comme une table
+// L'annotation @Table indique le nom de la table dans la base de données.
 @Entity
 @Table (name = "livre")
 public class Livre {
 
+    // L'annotation @Id spécifie la clé primaire d'une entité.
+    // GenerationType.IDENTITY indique auto-increment dans la base de données.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    // Déclaration des autres colonnes
     @Column (name = "TITRE", length = 255)
     private String title;
 
@@ -17,15 +22,20 @@ public class Livre {
     @Column (name = "AUTEUR", length = 50)
     private String author;
 
+    // Le constructeur par défaut (sans paramethes) requis par JPA pour créer une instance d'entité.
+    // Il est souvent utilisé lors de la récupération de données de la base de données.
     public Livre() {
     }
 
+    // Constructeur supplémentaire avec des paramètres pour initialiser l'entité avec des valeurs données.
+    // Cela peut être utile lors de la création de nouvelles instances de l'entité à enregistrer dans la base de données.
     public Livre(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
 
+    // getters
     public int getId() {
         return id;
     }
@@ -38,6 +48,7 @@ public class Livre {
         return author;
     }
 
+    // setters
     public void setId(int id) {
         this.id = id;
     }
@@ -50,6 +61,7 @@ public class Livre {
         this.author = author;
     }
 
+    // La méthode toString fournit une représentation sous forme de chaîne de l'objet Livre.
     @Override
     public String toString() {
         return "Livre{" +
